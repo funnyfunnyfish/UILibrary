26628ad67e5b6f46ca7f3de670f08cecf19d268e
nate <strangetad666@gmail.com>
2025-04-20 11:50:42 -0700

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,26 @@
+ 
+
+
+> Open this page at [https://funnyfunnyfish.github.io/uilibrary/](https://funnyfunnyfish.github.io/uilibrary/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/funnyfunnyfish/uilibrary** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/funnyfunnyfish/uilibrary** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,170 @@
+//made by carter
+console.consoleOverlay.consoleDisplay(true)
+
+// browserEvents.MouseLeft.onEvent(browserEvents.MouseButtonEvent.Pressed, function(x: number, y: number) {
+//     for (let item of uiElements) {
+//         if (item.boundingBox.IsPositionInBoundingBox(x, y)) {
+//             item.OnClick()
+//         }
+//     }
+// })
+
+class UIPage {
+    static instances: UIPage[] = []
+    public elements: UIElement[]
+    
+    constructor(elements: UIElement[]) {
+        this.elements = elements
+        UIPage.instances.push(this)
+    }
+
+    static SwitchScreen(newPage: UIPage) {
+        for (let page of UIPage.instances) {
+            page.HidePage()
+        }
+        newPage.ShowPage()
+    }
+
+    public HidePage() {
+        for (let element of this.elements) {
+            element.Hide()
+        }
+    }
+
+    public ShowPage() {
+        for (let element of this.elements) {
+            element.Show()
+        }
+    }
+}
+
+class UIElement {
+    static instances: UIElement[] = []
+    public boundingBox: BoundingBox
+    public sprite: Sprite
+    public onClick: Function
+    private isButton: boolean
+    private hidden: boolean
+
+    constructor(sprite: Sprite, onClick?: Function) {
+        this.sprite = sprite
+        this.boundingBox = new BoundingBox(sprite)
+
+        if (onClick != null) {
+            this.isButton = true
+            this.onClick = onClick
+        } else {
+            this.isButton = false
+        }
+        
+        this.hidden = false
+        UIElement.instances.push(this)
+    }
+
+    public OnClick() {
+        if (!this.hidden && this.isButton){
+            this.onClick()
+        }
+    }
+
+    public Hide() {
+        this.hidden = true
+        this.sprite.setFlag(SpriteFlag.Invisible, true)
+    }
+
+    public Show() {
+        this.hidden = false
+        this.sprite.setFlag(SpriteFlag.Invisible, false)
+    }
+}
+
+class BoundingBox {
+    public xMin: number
+    public xMax: number
+    public yMin: number
+    public yMax: number
+
+    constructor(sprite?: Sprite, xMin?: number, xMax?: number, yMin?: number, yMax?: number) {
+        if (!xMin || !xMax || !yMin || !yMax) {
+            this.CalculateBoundingBox(sprite)
+        } else {
+            this.xMin = xMin
+            this.xMax = xMax
+            this.yMin = yMin
+            this.yMax = yMax
+        }
+    }
+
+    public CalculateBoundingBox(sprite: Sprite) {
+        this.xMin = sprite.x - (sprite.width / 2)
+        this.xMax = sprite.x + (sprite.width / 2)
+        this.yMin = sprite.y - (sprite.height / 2)
+        this.yMax = sprite.y + (sprite.height / 2)
+    }
+
+    public IsPositionInBoundingBox(x: number, y: number): boolean {
+        return (x > this.xMin && x < this.xMax) && (y > this.yMin && y < this.yMax)
+    }
+}
+
+/* EXAMPLE BUTTON
+// Create sprite, any sprite works but bounding box is always going to be a rectangle
+let mySprite = sprites.create(img`dog`)
+
+// If you want your UI element to be a button, you must assign it a function
+let myFunc = () => { console.log("pressed") }
+
+// Creates UI element, and UI page to store element
+let myElement = new UIElement(mySprite, myFunc)
+let myPage = new UIPage([myElement])
+
+// Switches starting screen to be your page
+UIPage.SwitchScreen(myPage)
+*/
+
+// let mySprite = sprites.create(img`
+//     3333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333
+//     3111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111113
+//     3111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111113
+//     3111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111113
+//     3111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111113
+//     3111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111113
+//     3111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111113
+//     3111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111113
+//     3111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111113
+//     3111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111113
+//     3111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111113
+//     3111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111113
+//     3111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111113
+//     3111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111113
+//     3111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111113
+//     3333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333
+// `, SpriteKind.Player)
+// let mySprite2 = sprites.create(img`
+//     3333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333
+//     3111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111113
+//     3111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111113
+//     3111111111111111111111111111111111111111122211111111111111111111111111111111111111111111111111111113
+//     3111111111111111111111111111111111111111211121111111111111111111111111111111111111111111111111111113
+//     3111111111111111111111111111111111111111111112111111111111111111111111111111111111111111111111111113
+//     3111111111111111111111111111111111111111111112111111111111111111111111111111111111111111111111111113
+//     3111111111111111111111111111111111111111111112111111111111111111111111111111111111111111111111111113
+//     3111111111111111111111111111111111111111111112111111111111111111111111111111111111111111111111111113
+//     3111111111111111111111111111111111111111111121111111111111111111111111111111111111111111111111111113
+//     3111111111111111111111111111111111111111111221111111111111111111111111111111111111111111111111111113
+//     3111111111111111111111111111111111111111122211111111111111111111111111111111111111111111111111111113
+//     3111111111111111111111111111111111111111222222221111111111111111111111111111111111111111111111111113
+//     3111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111113
+//     3111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111113
+//     3333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333
+// `, SpriteKind.Player)
+// mySprite2.setPosition(80, 100)
+
+// const testFunction = () => {
+//     console.log("Hello you clicked space button")
+// }
+// const testFunction2 = () => {
+//     console.log("Hello you clicked second space button")
+// }
+
+// let uiElements: UIElement[] = [new UIElement(mySprite, testFunction), new UIElement(mySprite2, testFunction2)]
